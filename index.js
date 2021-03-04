ed91503df9ac494ef98ab5a167c4ceae82387bdc
Kouki Aoto <koukiaoto@KoukinoMacBook-Pro.local>
2021-03-04 17:07:47 +0900

    add font-family

---
+++ index.js
@@ -0,0 +1,29 @@
+let unko = "Hello";
+
+const bigUnko = "Helloooo";
+
+let arr = ["いーーーち", "にー", "さーん", "よん"]
+
+// let index = 0;
+// while( index < arr.length){
+//   console.log(arr[index]);
+//   index++;
+// }
+// if (arr.length > 5) {
+//   console.log("わーーーーーーーー");
+// }else {
+//   console.log("bonnba---");
+// }
+
+
+const test = (arg) => {
+  if (arr.length > arg) {
+    console.log("わーーーーーーーー");
+  }else {
+    console.log("bonnba---");
+  }
+}
+
+console.log(document.getElementsByTagName('button')[0].addEventListener('click',()=> {
+  window.alert("HelloWorld");
+}));
